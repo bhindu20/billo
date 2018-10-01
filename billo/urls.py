5bbaec9c914c81ebc61caef1b8dcdf3f117285e2
Marie Selvanadin <mks49@CNDL-AL-7AFVH8.home>
2018-09-30 22:02:07 -0400

    Added simple login form - need to connect to view

--- billo/urls.py
+++ billo/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.views.generic import TemplateView
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name='index.html'))
+    path('', TemplateView.as_view(template_name='index.html')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
